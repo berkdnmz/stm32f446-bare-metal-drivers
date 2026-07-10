86d0e42a6860f16721f975d6dd34308b5d73bcdd
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-07-11 00:11:29 +0300

    feat: add external LED/button validation with pull-up

---
+++ Src/003led_button_ext.c
@@ -0,0 +1,55 @@
+/*
+ * 003led_button_ext.c
+ *
+ *  Created on: Jul 10, 2026
+ *      Author: berkd
+ */
+
+
+#include "stm32f446xx.h"
+
+#define LOW 0
+#define BTN_PRESSED LOW
+
+void delay(void)
+{
+	for(uint32_t i = 0; i < 500000/2 ; i ++);
+}
+int main(void)
+{
+
+	GPIO_Handle_t GpioLed, GpioBtn;
+
+	GpioLed.pGPIOx = GPIOA;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_1;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+
+	GPIO_Init(&GpioLed);
+
+
+	GpioBtn.pGPIOx = GPIOA;
+	GpioBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	GpioBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GpioBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+	GPIO_Init(&GpioBtn);
+
+
+	while(1)
+	{
+		if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == BTN_PRESSED)
+		{
+			delay();
+			GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_1);
+		}
+	}
+
+	return 0;
+}
