5c5ad0da12b8ae960b532348349f851d565186c7
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-07-23 20:40:41 +0300

    feat(spi): implement SPI_Init and SPI_DeInit APIs with reset macros

--- drivers/Inc/stm32f446xx.h
+++ drivers/Inc/stm32f446xx.h
@@ -353,6 +353,13 @@ typedef struct
 #define GPIOG_REG_RESET()				do{ ( RCC->AHB1RSTR |= ( 1 << 6 ) ); (RCC->AHB1RSTR &= ~( 1 << 6)); }while(0)
 #define GPIOH_REG_RESET()				do{ ( RCC->AHB1RSTR |= ( 1 << 7 ) ); (RCC->AHB1RSTR &= ~( 1 << 7)); }while(0)
 
+/*
+ * Macros to reset SPIx peripherals
+ */
+#define SPI1_REG_RESET()				do{ ( RCC->APB2RSTR |= ( 1 << 12 ) ); (RCC->APB2RSTR &= ~( 1 << 12)); }while(0)
+#define SPI2_REG_RESET()				do{ ( RCC->APB1RSTR |= ( 1 << 14 ) ); (RCC->APB1RSTR &= ~( 1 << 14)); }while(0)
+#define SPI3_REG_RESET()				do{ ( RCC->APB1RSTR |= ( 1 << 15 ) ); (RCC->APB1RSTR &= ~( 1 << 15)); }while(0)
+#define SPI4_REG_RESET()				do{ ( RCC->APB2RSTR |= ( 1 << 13 ) ); (RCC->APB2RSTR &= ~( 1 << 13)); }while(0)
 
 /*
  * TODO: Refactor to static inline function
@@ -365,6 +372,7 @@ typedef struct
 										  (x == GPIOE)?4:\
 										  (x == GPIOF)?5:\
 										  (x == GPIOG)?6:0)
+// TODO: add GPIOH
 
 
 /*
@@ -409,7 +417,54 @@ typedef struct
 #define GPIO_PIN_SET					SET
 #define GPIO_PIN_RESET					RESET
 
-#include "stm32f446xx_gpio_driver.h"
 
+/***************************************************************************
+ * 				Bit position definitions of SPI peripheral
+ ***************************************************************************/
+/*
+ * Bit position definitions SPI_CR1
+ */
+#define SPI_CR1_CPHA      				0
+#define SPI_CR1_CPOL			        1
+#define SPI_CR1_MSTR    			    2
+#define SPI_CR1_BR       			   	3
+#define SPI_CR1_SPE      			   	6
+#define SPI_CR1_LSBFIRST    			7
+#define SPI_CR1_SSI         			8
+#define SPI_CR1_SSM         			9
+#define SPI_CR1_RXONLY      			10
+#define SPI_CR1_DFF         			11
+#define SPI_CR1_CRCNEXT     			12
+#define SPI_CR1_CRCEN       			13
+#define SPI_CR1_BIDIOE      			14
+#define SPI_CR1_BIDIMODE    			15
+
+/*
+ * Bit position definitions SPI_CR2
+ */
+#define SPI_CR2_RXDMAEN    				0
+#define SPI_CR2_TXDMAEN    				1
+#define SPI_CR2_SSOE       				2
+#define SPI_CR2_FRF        				4
+#define SPI_CR2_ERRIE      				5
+#define SPI_CR2_RXNEIE     				6
+#define SPI_CR2_TXEIE      				7
+
+/*
+ * Bit position definitions SPI_SR
+ */
+#define SPI_SR_RXNE       				0
+#define SPI_SR_TXE        				1
+#define SPI_SR_CHSIDE     				2
+#define SPI_SR_UDR        				3
+#define SPI_SR_CRCERR     				4
+#define SPI_SR_MODF       				5
+#define SPI_SR_OVR        				6
+#define SPI_SR_BSY        				7
+#define SPI_SR_FRE        				8
+
+
+#include "stm32f446xx_gpio_driver.h"
+#include "stm32f446xx_spi_driver.h"
 
 #endif /* INC_STM32F446XX_H_ */
--- drivers/Src/stm32f446xx_spi_driver.c
+++ drivers/Src/stm32f446xx_spi_driver.c
@@ -9,7 +9,7 @@
 #include "stm32f446xx_spi_driver.h"
 
 
-/**********************************************************************************************
+/*************************************************************************************************************************
  * @fn 					- SPI_PeriClockControl
  *
  * @brief				- This function enables or disables the peripheral clock for the given SPI peripheral.
@@ -21,7 +21,7 @@
  *
  * @Note				- The peripheral clock must be enabled before configuring SPI registers or calling SPI_Init().
  *
- ***********************************************************************************************/
+ *************************************************************************************************************************/
 void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
 {
 	if(EnorDi == ENABLE)
@@ -43,6 +43,95 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
 	else
 	{
 		//TODO
+	}
+}
+
+/*************************************************************************************************************************
+ * @fn 					- SPI_Init
+ *
+ * @brief				- This function initializes the given SPI peripheral with the user settings.
+ *
+ * @param[in]			- pSPIHandle : Pointer to the SPI_Handle_t structure containing configuration parameters.
+ *
+ * @return				- None.
+ *
+ * @Note				- None.
+ *
+ *************************************************************************************************************************/
+void SPI_Init(SPI_Handle_t *pSPIHandle)
+{
+	 //first lets configure the SPI_CR1 register
+
+	uint32_t tempreg = 0;
+
+	//1. configure the device mode
+	tempreg |= pSPIHandle->SPIConfig.SPI_DeviceMode << SPI_CR1_MSTR;
+
+	//2. configure the bus config
+	if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+	{
+		//BIDI mode should be cleared
+		tempreg &= ~( 1 << SPI_CR1_BIDIMODE );
+	}else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+	{
+		//BIDI mode should be set
+		tempreg |= ( 1 << SPI_CR1_BIDIMODE );
+	}else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
+	{
+		//BIDI mode should be cleared
+		tempreg &= ~( 1 << SPI_CR1_BIDIMODE );
+		//RXONLY bit must be set
+		tempreg |= ( 1 << SPI_CR1_RXONLY );
+	}
+
+	//3. configure the SPI serial clock speed (baud rate)
+	tempreg |= pSPIHandle->SPIConfig.SPI_SclkSpeed << SPI_CR1_BR;
+
+	//4. configure the DFF
+	tempreg |= pSPIHandle->SPIConfig.SPI_DFF << SPI_CR1_DFF;
+
+	//5. configure the CPOL
+	tempreg |= pSPIHandle->SPIConfig.SPI_CPOL << SPI_CR1_CPOL;
+
+	//6. configure the CPHA
+	tempreg |= pSPIHandle->SPIConfig.SPI_CPHA << SPI_CR1_CPHA;
+
+	//7. configure the SSM
+	tempreg |= pSPIHandle->SPIConfig.SPI_SSM << SPI_CR1_SSM;
+
+	pSPIHandle->pSPIx->CR1 = tempreg;
+
+}
+
+/*************************************************************************************************************************
+ * @fn 					- SPI_DeInit
+ *
+ * @brief				- Resets all the registers of the given SPI peripheral to their default state.
+ *
+ * @param[in]			- pSPIx : Base address of the SPI peripheral (e.g., SPI1, SPI2, SPI3, SPI4).
+ *
+ * @return				- None.
+ *
+ * @Note				- Uses RCC peripheral reset macros to reset the hardware logic.
+ *
+ *************************************************************************************************************************/
+void SPI_DeInit(SPI_RegDef_t *pSPIx)
+{
+	if(pSPIx == SPI1)
+	{
+		SPI1_REG_RESET();
+	}
+	else if(pSPIx == SPI2)
+	{
+		SPI2_REG_RESET();
+	}
+	else if(pSPIx == SPI3)
+	{
+		SPI3_REG_RESET();
+	}
+	else if(pSPIx == SPI4)
+	{
+		SPI4_REG_RESET();
 	}
 }
 
