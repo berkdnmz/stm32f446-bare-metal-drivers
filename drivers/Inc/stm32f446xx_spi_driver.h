ede426aa7458e443945af7e08c1a278424930c1c
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-07-29 10:34:54 +0300

    fix(spi): add SPI_GetFlagStatus prototype to header

--- drivers/Inc/stm32f446xx_spi_driver.h
+++ drivers/Inc/stm32f446xx_spi_driver.h
@@ -151,6 +151,7 @@ void SPI_IRQHandling(SPI_Handle_t *pSPIHandle);
 /*
  * Other Peripheral Control APIs
  */
+uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t FlagName);
 void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
 void SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
 void SPI_SSOEConfig(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
