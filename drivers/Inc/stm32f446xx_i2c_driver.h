50c9d38fd7b790c8b28d938e890416866e8f31ae
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-07-30 00:45:04 +0300

    feat(i2c): add I2C_Config_t and I2C_Handle_t structures

--- drivers/Inc/stm32f446xx_i2c_driver.h
+++ drivers/Inc/stm32f446xx_i2c_driver.h
@@ -10,4 +10,24 @@
 
 #include "stm32f446xx.h"
 
+/*
+ * Configuration structure for I2Cx peripheral
+ */
+typedef struct
+{
+	uint32_t I2C_SCLSpeed;
+	uint8_t I2C_DeviceAdress;
+	uint8_t I2C_ACKControl;
+	uint8_t I2C_FMDutyCycle;
+}I2C_Config_t;
+
+/*
+ * Handle structure for I2Cx peripheral
+ */
+typedef struct
+{
+	I2C_RegDef_t *pI2Cx;
+	I2C_Config_t I2C_Config;
+}I2C_Handle_t;
+
 #endif /* INC_STM32F446XX_I2C_DRIVER_H_ */
