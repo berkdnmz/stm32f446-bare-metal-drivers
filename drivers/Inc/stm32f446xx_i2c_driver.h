3dec12296e296abd86c40f43cb13d545963eebdc
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-07-29 22:39:10 +0300

    feat(i2c): add I2C header and source skeleton files

---
+++ drivers/Inc/stm32f446xx_i2c_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f446xx_i2c_driver.h
+ *
+ *  Created on: Jul 29, 2026
+ *      Author: berkd
+ */
+
+#ifndef INC_STM32F446XX_I2C_DRIVER_H_
+#define INC_STM32F446XX_I2C_DRIVER_H_
+
+#include "stm32f446xx.h"
+
+#endif /* INC_STM32F446XX_I2C_DRIVER_H_ */
---
+++ drivers/Src/stm32f446xx_i2c_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f446xx_i2c_driver.c
+ *
+ *  Created on: Jul 29, 2026
+ *      Author: berkd
+ */
+
+#include"stm32f446xx_i2c_driver.h"
