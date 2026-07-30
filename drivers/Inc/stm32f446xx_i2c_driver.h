2b27ec699aa2bab806d1266194098acf6a97c244
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-07-30 13:53:50 +0300

    feat(i2c): add driver API prototypes and implement I2C_PeriClockcontrol

--- drivers/Inc/stm32f446xx_i2c_driver.h
+++ drivers/Inc/stm32f446xx_i2c_driver.h
@@ -49,4 +49,40 @@ typedef struct
 #define I2C_FM_DUTY_2						0
 #define I2C_FM_DUTY_16_9					1
 
+
+/******************************************************************************************
+ * 								APIs supported by this driver
+ * 			For more information about the APIs check the function definitions
+ ******************************************************************************************/
+/*
+ * Peripheral Clock Setup
+ */
+void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
+
+/*
+ * Init and De-init
+ */
+void I2C_Init(I2C_Handle_t *pI2CHandle);
+void I2C_DeInit(I2C_RegDef_t *pI2Cx);
+
+/*
+ * Data Send and Receive
+ */
+
+/*
+ * IRQ Configuration and ISR handling
+ */
+void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void I2C_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+
+/*
+ * Other Peripheral Control APIs
+ */
+uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName);
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
+/*
+ * Application callback
+ */
+void I2C_ApplicationEventCallback(I2C_Handle_t *pI2CHandle, uint8_t AppEv);
+
 #endif /* INC_STM32F446XX_I2C_DRIVER_H_ */
--- drivers/Src/stm32f446xx_i2c_driver.c
+++ drivers/Src/stm32f446xx_i2c_driver.c
@@ -6,3 +6,48 @@
  */
 
 #include"stm32f446xx_i2c_driver.h"
+
+/*************************************************************************************************************************
+ * @fn 					- I2C_PeriClockControl
+ *
+ * @brief				- This function enables or disables the peripheral clock for the given I2C peripheral.
+ *
+ * @param[in]			- pI2Cx : Base address of the I2C peripheral (e.g., I2C1, I2C2, I2C3).
+ * @param[in]			- EnorDi : ENABLE or DISABLE macro to control the clock.
+ *
+ * @return				- None.
+ *
+ * @Note				- None.
+ *
+ *************************************************************************************************************************/
+void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pI2Cx == I2C1)
+		{
+			I2C1_PCLK_EN();
+		}else if(pI2Cx == I2C2)
+		{
+			I2C2_PCLK_EN();
+		}else if(pI2Cx == I2C3)
+		{
+			I2C3_PCLK_EN();
+		}
+	}
+	else
+	{
+		if (pI2Cx == I2C1)
+		{
+			I2C1_PCLK_DI();
+		}
+		else if (pI2Cx == I2C2)
+		{
+			I2C2_PCLK_DI();
+		}
+		else if (pI2Cx == I2C3)
+		{
+			I2C3_PCLK_DI();
+		}
+	}
+}
