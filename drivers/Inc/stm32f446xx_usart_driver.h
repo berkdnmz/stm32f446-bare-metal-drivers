514a236d1c862364f9ce0cfb77cec5d12a28d63f
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-08-17 10:41:34 +0300

    feat(usart): add USART header and source skeleton files

---
+++ drivers/Inc/stm32f446xx_usart_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f446xx_usart_driver.h
+ *
+ *  Created on: Aug 17, 2026
+ *      Author: berkd
+ */
+
+#ifndef INC_STM32F446XX_USART_DRIVER_H_
+#define INC_STM32F446XX_USART_DRIVER_H_
+
+#include "stm32f446xx.h"
+
+#endif /* INC_STM32F446XX_USART_DRIVER_H_ */
---
+++ drivers/Src/stm32f446xx_usart_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f446xx_usart_driver.c
+ *
+ *  Created on: Aug 17, 2026
+ *      Author: berkd
+ */
+
+#include "stm32f446xx_usart_driver.h"
