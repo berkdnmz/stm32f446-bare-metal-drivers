daa8fb86a523ffbee67a4bb782875d52a503b069
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-08-17 11:53:13 +0300

    feat(usart): implement USART_PeriClockControl API

--- drivers/Src/stm32f446xx_usart_driver.c
+++ drivers/Src/stm32f446xx_usart_driver.c
@@ -6,3 +6,76 @@
  */
 
 #include "stm32f446xx_usart_driver.h"
+
+#include "stm32f446xx_usart_driver.h"
+
+/*************************************************************************************************************************
+ * @fn                  - USART_PeriClockControl
+ *
+ * @brief               - This function enables or disables the peripheral clock for the given USART/UART peripheral.
+ *
+ * @param[in]           - pUSARTx : Base address of the USART/UART peripheral (e.g., USART1, USART2, USART3, UART4, UART5, USART6).
+ * @param[in]           - EnorDi  : ENABLE or DISABLE macro to control the clock.
+ *
+ * @return              - None.
+ *
+ * @Note                - None.
+ *
+ *************************************************************************************************************************/
+void USART_PeriClockControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi)
+{
+    if(EnorDi == ENABLE)
+    {
+        if(pUSARTx == USART1)
+        {
+            USART1_PCLK_EN();
+        }
+        else if(pUSARTx == USART2)
+        {
+            USART2_PCLK_EN();
+        }
+        else if(pUSARTx == USART3)
+        {
+            USART3_PCLK_EN();
+        }
+        else if(pUSARTx == UART4)
+        {
+            UART4_PCLK_EN();
+        }
+        else if(pUSARTx == UART5)
+        {
+            UART5_PCLK_EN();
+        }
+        else if(pUSARTx == USART6)
+        {
+            USART6_PCLK_EN();
+        }
+    }
+    else
+    {
+        if(pUSARTx == USART1)
+        {
+            USART1_PCLK_DI();
+        }
+        else if(pUSARTx == USART2)
+        {
+            USART2_PCLK_DI();
+        }
+        else if(pUSARTx == USART3)
+        {
+            USART3_PCLK_DI();
+        }
+        else if(pUSARTx == UART4)
+        {
+            UART4_PCLK_DI();
+        }
+        else if(pUSARTx == UART5)
+        {
+            UART5_PCLK_DI();
+        }
+        else if(pUSARTx == USART6)
+        {
+            USART6_PCLK_DI();
+        }
+    }
+}
